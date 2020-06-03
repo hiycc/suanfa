7b79c14427d2830256dbe0a4bd3f4c3f8fdc9803
Luxi <mac@Luxis-MacBook-Pro.local>
2020-06-03 17:01:25 +0800

    Add a new project

---
+++ dynamic/Cut_steel/cmake-build-debug/CMakeFiles/Cut_steel.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Cut_steel.dir/main.cpp.o"
+  "Cut_steel"
+  "Cut_steel.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Cut_steel.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ dynamic/Cut_steel/cmake-build-debug/CMakeFiles/Cut_steel.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Cut_steel.
+# This may be replaced when dependencies are built.
