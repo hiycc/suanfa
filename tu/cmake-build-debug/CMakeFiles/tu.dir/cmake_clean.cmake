f98a35fc1de73b8e4d68b2c7a0fceb5fe373cd0c
Luxi <hiycc@outlook.com>
2020-06-20 23:58:54 +0800

    create a project tu

---
+++ tu/cmake-build-debug/CMakeFiles/tu.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tu.dir/main.cpp.o"
+  "tu"
+  "tu.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tu.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
